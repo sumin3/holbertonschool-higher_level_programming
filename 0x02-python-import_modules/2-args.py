8fe69341154132d3f9be64d9a1bad0c65e4c5db2
sumin3 <suminyu@yahoo.com>
2018-09-13 19:53:03 +0000

    print . instead of : when argument is 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} arguments:".format(len(sys.argv) - 1))
+    c = ':'
+    if (len(sys.argv) == 1):
+        c = '.'
+    print("{:d} arguments{:s}".format(len(sys.argv) - 1, c))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
