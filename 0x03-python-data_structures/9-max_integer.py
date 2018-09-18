70453f342e9174388a2d0e40e60bf75fc5276b24
sumin3 <suminyu@yahoo.com>
2018-09-18 23:01:20 +0000

    add new file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    max_num = 0
+    for num in my_list:
+        if num >= max_num:
+            max_num = num
+    return max_num
