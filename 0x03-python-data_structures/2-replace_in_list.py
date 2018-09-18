de0ae3348c19da94b60b4a2e5ae932ddf9bb7f7c
sumin3 <suminyu@yahoo.com>
2018-09-18 20:30:31 +0000

    remove case check

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,4 @@
 def replace_in_list(my_list, idx, element):
-    elements = len(my_list)
-    if idx < 0:
-        return my_list
-    if idx > elements:
-        return my_list
-    my_list[idx] = element
+    if idx > 0 and idx < len(my_list):
+        my_list[idx] = element
     return my_list
