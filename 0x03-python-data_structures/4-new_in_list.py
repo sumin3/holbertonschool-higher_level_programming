a02c9c5e24c703d8a5c6226c4e333eb53fd54df8
sumin3 <suminyu@yahoo.com>
2018-09-18 20:57:33 +0000

    check if the list is empty

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list):
+    if my_list is None:
+        return None
+    if idx < 0 or idx >= len(my_list):
         return my_list
     new_list = []
     for i in my_list[:]:
