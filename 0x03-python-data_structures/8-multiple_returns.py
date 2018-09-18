e516b32fb1fd862fd738b3d2379c8c3df944a476
sumin3 <suminyu@yahoo.com>
2018-09-18 22:55:05 +0000

    change if condiction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return 0, None
+    if len(sentence) == 0:
+        return len(sentence), None
     else:
         return len(sentence), sentence[0]
