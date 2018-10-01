a19c05fd56064c24ede07526733d8458cfbb7e2a
sumin3 <suminyu@yahoo.com>
2018-10-01 22:16:31 +0000

    directly print number instead of convert

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    s = ""
-    s1 = ""
     len = 0
     try:
         for i in range(0, x):
-            s += str(my_list[i])
-        num = int(s)
-        print(s)
+            print("{:d}".format(my_list[i]), end="")
+        print()
         return (i + 1)
     except (IndexError, TypeError, ValueError):
         for n in my_list:
-            s1 += str(n)
             len += 1
-        num = int(s1)
-        print(num)
+        print()
         return (len)
