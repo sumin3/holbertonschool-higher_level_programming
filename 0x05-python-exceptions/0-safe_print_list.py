940ba2fd84c27875ad8f3d2f18a05d0be8a43377
sumin3 <suminyu@yahoo.com>
2018-10-01 22:23:06 +0000

    rm print in except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
-        print()
-        return (i + 1)
-    except (IndexError, TypeError, ValueError):
-        for n in my_list:
             len += 1
-        print()
-        return (len)
+    except (IndexError, TypeError, ValueError):
+        pass
+    print()
+    return len
