818b2d565d364589a7b3d03e609c810b46dc3ad5
sumin3 <suminyu@yahoo.com>
2018-10-01 21:41:28 +0000

    use finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {}".format(result))
-        return result
     except (ZeroDivisionError, ValueError):
         print("Inside result: None")
-        return None
+        result =  None
+    finally:
+        return result
