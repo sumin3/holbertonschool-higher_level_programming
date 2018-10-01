3e0a74c3ff65583c9755814574abd925c5ca59ae
sumin3 <suminyu@yahoo.com>
2018-10-01 22:04:08 +0000

    add 2 error check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         num = int(s)
         print(s)
         return (i + 1)
-    except (IndexError):
+    except (IndexError, TypeError, ValueError):
         for n in my_list:
             s1 += str(n)
             len += 1
