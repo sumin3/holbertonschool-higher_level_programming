76da42e2e9ba9df2be1e827424477a4fa3837937
sumin3 <suminyu@yahoo.com>
2018-10-03 02:40:18 +0000

    rm '__' in init function

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,8 +18,8 @@ class Square:
              size: size of square.
              position: position of square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
