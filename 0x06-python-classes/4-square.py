f09f4db815f9c3c301401ec86bae497ed2c6764e
sumin3 <suminyu@yahoo.com>
2018-10-03 02:45:15 +0000

    refactor my code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,13 +19,7 @@ class Square:
              TypeError: size must be an integer
              ValueError: size must be >= 0
         """
-        if isinstance(size, int):
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
+        self.size = size
 
     @property
     def size(self):
