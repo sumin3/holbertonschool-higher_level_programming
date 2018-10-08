66de637133462b6859aa00bc7f57ae2392eecff8
sumin3 <suminyu@yahoo.com>
2018-10-08 09:15:07 +0000

    rm print when size is 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,7 +15,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(0, size):
+    for i in range(size):
         print("{}".format('#' * size))
-    if size == 0:
-        print()
+
