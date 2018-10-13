568bad334173b345adeb7db06ad0f6b5e2ce35d1
sumin3 <suminyu@yahoo.com>
2018-10-13 23:58:22 +0000

    check is NaN

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,21 +7,20 @@ Module to find the max integer in a list
 def add_integer(a, b=98):
     """ This is a function that add two integers
     Args:
-       a: first integer that need to add
-       b: second integer that need to add
+    a: first integer that need to add
+    b: second integer that need to add
     Return:
-        return: sum
+    return: sum
     """
-
-    if not isinstance(a, (float, int)):
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if a is None or a is not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (float, int)):
+    if b is None or a is not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a + b)
 
 if __name__ == '__main__':
     import doctest
