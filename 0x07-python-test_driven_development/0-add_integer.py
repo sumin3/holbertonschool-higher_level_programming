0dde7e0a66489378076e27dc0832576696bbcaa7
sumin3 <suminyu@yahoo.com>
2018-10-14 00:45:34 +0000

    add overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,12 +16,13 @@ def add_integer(a, b=98):
         a = 89
     if b != b:
         b = 89
-    if a is None:
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    sum = a + b
+    if sum == float('inf') or sum == -float('inf'):
+        return 89
     return int(a) + int(b)
 
 if __name__ == '__main__':
