cf21a5932e4d1f5ef190c2df678e96525a1ce2b9
sumin3 <suminyu@yahoo.com>
2018-10-17 05:38:15 +0000

    add new file

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """returns a list of lists of integers representing the
+    Pascal’s triangle of n
+    Args:
+        n: the size of triangle
+    Return:
+        return a matrix of pascal's triangle
+    """
+    p_tri = [[1]]
+    list = [1, 1]
+
     if n <= 0:
         return []
-    p_tri = [[1], [1,1]]
-    list = [1,1]
-    prev_list = [1,1]
     for i in range(1, n):
         prev_list = list
         list = [1]
-        print("i", i)
-        print("len", len(prev_list))
-        for j in range(1, i-1):
+        for j in range(0, i-1):
             list.append(prev_list[j] + prev_list[j + 1])
-        print("end")
         list.append(1)
         p_tri.append(list)
     return p_tri
