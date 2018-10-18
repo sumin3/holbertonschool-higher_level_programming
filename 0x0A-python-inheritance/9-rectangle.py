7229401b98a30a806879b1b1546728553a5d53c8
sumin3 <suminyu@yahoo.com>
2018-10-18 07:34:04 +0000

    import class instead of copy the class to the same file

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """BaseGeometry class"""
-    def area(self):
-        """area function, raise exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value
-        Args:
-            name: the name
-            value: integer value
-        Raises:
-            TypeError: <name> must be an integer
-            ValueError: <name> must be greater than 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""import class"""
 
 
 class Rectangle(BaseGeometry):
