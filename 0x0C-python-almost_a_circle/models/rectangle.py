0d5eaff7ffc1e019675b0c293915ac89df4b0932
sumin3 <suminyu@yahoo.com>
2018-10-21 04:17:06 +0000

    use type instead of isinstance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,7 +62,7 @@ class Rectangle(Base):
                TypeError: height must be an integer
                ValueError: height must be >= 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
