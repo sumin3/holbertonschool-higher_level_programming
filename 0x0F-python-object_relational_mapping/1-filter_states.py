bac38835f6877e9347c4838c6631ad90553e9ddd
sumin3 <suminyu@yahoo.com>
2019-01-04 01:38:25 +0000

    remove db.close

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3]
+    )
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
     WHERE name LIKE 'N%' ORDER BY id;""")
     list_states = cur.fetchall()
     for state in list_states:
         print("{}".format(state))
-    db.close()
