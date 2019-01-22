1782e50a601d2579fe4486860e82932cba93baff
sumin3 <suminyu@yahoo.com>
2019-01-22 01:43:24 +0000

    init commit

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# takes in a URL, sends a request to the URL and displays the value
+# of the X-Request-Id variable found in the header of the response
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
