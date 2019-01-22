9b2904c61fcc918e0a705a25f95138d80795df07
sumin3 <suminyu@yahoo.com>
2019-01-22 01:50:21 +0000

    move import outside of if main

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 takes in a URL, sends a request to the URL and displays the value
  of the X-Request-Id variable found in the header of the response
 """
+import requests
+import sys
+
 if __name__ == "__main__":
-    import requests
-    import sys
 
     r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
