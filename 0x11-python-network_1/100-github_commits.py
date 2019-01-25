110f43209252cbe6fdbdd5667d04e46b83735ad0
sumin3 <suminyu@yahoo.com>
2019-01-25 21:16:26 +0000

    init commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ a Python script that takes 2 arguments that list 10 commits (from
+the most recent to oldest) of the repository “rails” by the user “rails”
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    from pprint import pprint
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    r = requests.get(url)
+    json = r.json()
+    count = 1
+    for i in json:
+        print(i.get('sha'), i.get('commit').get('author').get('name'))
+        if count == 10:
+            break
+        count += 1
