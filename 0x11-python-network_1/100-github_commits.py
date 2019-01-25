964501b0e37affc02d9980834234d0ebeb00bc35
sumin3 <suminyu@yahoo.com>
2019-01-25 21:33:46 +0000

    change argv order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,15 +7,12 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     r = requests.get(url)
     json = r.json()
     count = 1
     for i in json:
-        try:
-            print(i.get('sha') + ':', i.get('commit').get('author').get('name'))
-        except:
-            print('None')
+        print(i.get('sha') + ':', i.get('commit').get('author').get('name'))
         if count == 10:
             break
         count += 1
