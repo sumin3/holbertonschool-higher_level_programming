ba82890bc0ad391fca56415e195c1e1654aeb9c3
sumin3 <suminyu@yahoo.com>
2019-02-12 23:15:50 +0000

    change comment

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-/* prints the first argument passed to it */
+/* two arguments passed to it, in the following format: “ is ”
+*/
 if (!process.argv[2] && !process.argv[3]) {
   console.log('undefined is undefined');
 } else if (process.argv[2] && !process.argv[3]) {
