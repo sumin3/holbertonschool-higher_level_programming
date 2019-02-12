054fd98c3c4dfeeda98fe80ed2ef4dba90e74018
sumin3 <suminyu@yahoo.com>
2019-02-12 23:18:11 +0000

    remove space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ if the first argument can be converted to an integer
 if (!process.argv[2]) {
   console.log('No argument');
 } else if (parseInt(process.argv[2])) {
-  console.log('My number: ', parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
