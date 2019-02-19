0711e167f4f6472811761945b57049c47d16ed96
sumin3 <suminyu@yahoo.com>
2019-02-19 23:12:40 +0000

    change comment

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-/* display the status code of a GET request. */
+/* title of a Star Wars movie where the episode 
+number matches a given integer. */
 const request = require('request');
 const url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
