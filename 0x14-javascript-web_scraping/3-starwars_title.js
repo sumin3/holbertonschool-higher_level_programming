ebbb7d7b9221e6658986a5d9f88ece9eb9321df9
sumin3 <suminyu@yahoo.com>
2019-02-19 23:10:36 +0000

    init commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* display the status code of a GET request. */
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
